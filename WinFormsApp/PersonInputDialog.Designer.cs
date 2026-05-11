ef0c546a7e94078847ccd44b2cee36c265f2eccf
zenin5479 <zenin5479@gmail.com>
2026-05-11 20:30:13 +0300

    Update 1.4 Обновлен метод. Проведен рефакторинг

--- WinFormsApp/PersonInputDialog.Designer.cs
+++ WinFormsApp/PersonInputDialog.Designer.cs
@@ -85,6 +85,7 @@
          btnOk.TabIndex = 15;
          btnOk.Text = "Ok";
          btnOk.UseVisualStyleBackColor = true;
+         btnOk.Click += btnOk_Click_1;
          // 
          // txtSkills
          // 
--- WinFormsApp/PersonInputDialog.cs
+++ WinFormsApp/PersonInputDialog.cs
@@ -1,18 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+using System.Linq;
 using System.Windows.Forms;
+using ClassLibrary;
 
 namespace WinFormsApp
 {
    public partial class PersonInputDialog : Form
    {
-      public PersonInputDialog()
+      public Person CreatedPerson { get; private set; }
+      private Person _editPerson;
+
+      public PersonInputDialog(Person editPerson = null)
       {
          InitializeComponent();
+
+         _editPerson = editPerson;
+
+         if (_editPerson != null)
+         {
+            Text = "Редактирование человека";
+            txtName.Text = _editPerson.Name;
+            dtpBirth.Value = _editPerson.BirthDate;
+            numSalary.Value = _editPerson.Salary;
+            txtSkills.Text = string.Join(", ", _editPerson.Skills);
+            btnOk.Text = "Сохранить";
+         }
+         else
+         {
+            Text = "Добавление человека";
+            btnOk.Text = "Добавить";
+         }
+      }
+
+      private void btnOk_Click_1(object sender, EventArgs e)
+      {
+         if (string.IsNullOrWhiteSpace(txtName.Text))
+         {
+            MessageBox.Show("Введите имя!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+         }
+
+         if (_editPerson != null)
+         {
+            // Редактирование существующего
+            _editPerson.Name = txtName.Text;
+            _editPerson.BirthDate = dtpBirth.Value;
+            _editPerson.Salary = numSalary.Value;
+            _editPerson.Skills.Clear();
+            foreach (var skill in txtSkills.Text.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+               _editPerson.Skills.Add(skill.Trim());
+            CreatedPerson = _editPerson;
+         }
+         else
+         {
+            // Создание нового
+            CreatedPerson = new Person
+            {
+               Name = txtName.Text,
+               BirthDate = dtpBirth.Value,
+               Salary = numSalary.Value,
+               Skills = txtSkills.Text.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                  .Select(s => s.Trim()).ToList()
+            };
+         }
+
+         DialogResult = DialogResult.OK;
+         Close();
       }
    }
 }
