cf0be3b37afdd6d20b87131945d34fe9c4049a0e
zenin5479 <zenin5479@gmail.com>
2026-05-11 20:15:08 +0300

    Update 0.9 Обновлен метод. Проведен рефакторинг

--- ClassLibrary/Person.cs
+++ ClassLibrary/Person.cs
@@ -1,7 +1,20 @@
-﻿namespace ClassLibrary
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
 {
    public class Person
    {
-      
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public DateTime BirthDate { get; set; }
+      public decimal Salary { get; set; }
+      public List<string> Skills { get; set; } = new List<string>();
+
+      public override string ToString()
+      {
+         return string.Format("[{0}] {1} ({2:yyyy-MM-dd}) - ЗП: {3:C} - Навыки: {4}", Id, Name, BirthDate, Salary,
+            string.Join(", ", Skills));
+      }
    }
 }
