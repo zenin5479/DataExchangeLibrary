12a1e9540947bb25baf13c5b068bc77433cbaf9a
zenin5479 <zenin5479@gmail.com>
2026-05-11 20:28:24 +0300

    Update 1.3 Обновлен метод. Проведен рефакторинг

---
+++ WinFormsApp/PersonEditDialog.cs
@@ -0,0 +1,10 @@
+﻿using ClassLibrary;
+
+namespace WinFormsApp
+{
+   // PersonEditDialog можно удалить, используя PersonInputDialog с параметром
+   public class PersonEditDialog : PersonInputDialog
+   {
+      public PersonEditDialog(Person person) : base(person) { }
+   }
+}
