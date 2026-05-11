5afe88e85521de27fa926f243837e18dfe9670dd
zenin5479 <zenin5479@gmail.com>
2026-05-11 20:24:40 +0300

    Update 1.2 Обновлен метод. Проведен рефакторинг

---
+++ WinFormsApp/PersonInputDialog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+   public partial class PersonInputDialog : Form
+   {
+      public PersonInputDialog()
+      {
+         InitializeComponent();
+      }
+   }
+}
